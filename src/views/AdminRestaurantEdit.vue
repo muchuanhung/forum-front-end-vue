f9c69c2fe7b00553e02c6efe17eab86557b8082c
muchuan <mu.chuan.hung@gmail.com>
2021-11-27 22:51:59 +0800

    feat: Add functions to fetch adminrestaurantedit data from API

--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -1,97 +1,99 @@
+<!--新增 isProcessing 屬性避免使用者連續點擊-->
 <template>
   <div class="container py-5">
     <AdminRestaurantForm 
      :initial-restaurant="restaurant"
+     :is-processing="isProcessing"
      @after-submit="handleAfterSubmit" 
     />
   </div>
 </template>
 
 <script>
-import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
+import AdminRestaurantForm from './../components/AdminRestaurantForm'
+// STEP 1: 載入 adminAPI 和 Toast
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
 
-//設定種子資料
-const dummyData = {
-  'restaurant': {
-    'id': 1,
-    'name': 'Laurence Reynolds',
-    'tel': '1-657-067-3756 x9782',
-    'address': '187 Kirlin Squares',
-    'opening_hours': '08:00',
-    'description': 'sit est mollitia',
-    'image': 'https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887',
-    'viewCounts': 1,
-    'createdAt': '2019-07-30T16:24:55.432Z',
-    'updatedAt': '2019-07-30T17:26:43.260Z',
-    'CategoryId': 3,
-    'Category': {
-      'id': 3,
-      'name': '義大利料理',
-      'createdAt': '2019-07-30T16:24:55.429Z',
-      'updatedAt': '2019-07-30T16:24:55.429Z'
-    }
-  }
-}
 
+//這裡我們透過 this.$route.params 取得網址上的 id，才會知道現在編輯的是哪一筆餐廳資料
 export default {
   components: {
-    AdminRestaurantForm
+    AdminRestaurantForm,
   },
-
-   data () {
+  data() {
     return {
       restaurant: {
         id: -1,
-        name: '',
-        categoryId: '',
-        tel: '',
-        address: '',
-        description: '',
-        image: '',
-        openingHours: ''
-      }
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: "",
+      },
+      isProcessing: false
     }
   },
-  //這裡我們透過 this.$route.params 取得網址上的 id，才會知道現在編輯的是哪一筆餐廳資料
-
-    created () {
+  created() {
     const { id } = this.$route.params
     this.fetchRestaurant(id)
   },
-  methods: {
-     fetchRestaurant (restaurantId) {
-      console.log('fetchRestaurant id:', restaurantId)
-      const { restaurant } = dummyData
-      //用解構賦值
-      const {
-        id,
-        name,
-        Category,
-        image,
-        opening_hours:openingHours,
-        tel,
-        address,
-        description
-       } = restaurant
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        //if 存在回傳name else未分類
-        categoryId: Category ? Category.Id : '未分類',
-        tel,
-        address,
-        description,
-        image,
-        openingHours
+  beforeRouteUpdate(to, from, next) {
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
+ methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId })
+        const {
+          id,
+          name,
+          CategoryId: categoryId,
+          tel,
+          address,
+          description,
+          image,
+          opening_hours: openingHours,
+        } = data.restaurant
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          categoryId,
+          tel,
+          address,
+          description,
+          image,
+          openingHours,
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     },
-    handleAfterSubmit (formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async handleAfterFormSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const { data } = await adminAPI.restaurants.updateDetail({ restaurantId: this.restaurant.id, formData })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        //更新完成後轉址到 /admin/restaurants 頁面
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新餐廳資料，請稍後再試'
+        })
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
