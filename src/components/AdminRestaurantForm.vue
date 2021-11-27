639faf0ee3f48bc1efd3e3adf3832d7ccaf508d1
muchuan <mu.chuan.hung@gmail.com>
2021-11-27 17:49:33 +0800

    feat: add function to get admin restaurant data from API

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent.stop="handleFormSubmit">
+  <form @submit.prevent.stop="handleFormSubmit" v-show="!isLoading">
     <div class="form-group my-3">
       <label for="name">Name</label>
       <input
@@ -13,17 +13,17 @@
       />
     </div>
 
-    <div class="form-group">
+    <div class="form-group my-3">
       <label for="categoryId">Category</label>
       <select
-        id="categoryId"
         v-model="restaurant.categoryId"
-        class="form-control"
+        id="categoryId"
+        class="form-control mt-2"
         name="categoryId"
         required
-      >     
-         <option value="" selected disabled>--請選擇--</option>
-         <option
+      >
+        <option value="" selected disabled>--請選擇--</option>
+        <option
           v-for="category in categories"
           :key="category.id"
           :value="category.id"
@@ -33,77 +33,73 @@
       </select>
     </div>
 
-     <div class="form-group">
+    <div class="form-group my-3">
       <label for="tel">Tel</label>
       <input
-        id="tel"
         v-model="restaurant.tel"
+        id="tel"
         type="text"
-        class="form-control"
+        class="form-control mt-2"
         name="tel"
         placeholder="Enter telephone number"
-      >
+      />
     </div>
 
-    <div class="form-group">
+    <div class="form-group my-3">
       <label for="address">Address</label>
       <input
-        id="address"
         v-model="restaurant.address"
+        id="address"
         type="text"
-        class="form-control"
+        class="form-control mt-2"
         placeholder="Enter address"
         name="address"
-      >
+      />
     </div>
 
-    <div class="form-group">
+    <div class="form-group my-3">
       <label for="opening-hours">Opening Hours</label>
       <input
-        id="opening-hours"
         v-model="restaurant.openingHours"
+        id="opening-hours"
         type="time"
-        class="form-control"
+        class="form-control mt-2"
         name="opening_hours"
-      >
+      />
     </div>
 
-    <div class="form-group">
+    <div class="form-group my-3">
       <label for="description">Description</label>
       <textarea
-        id="description"
         v-model="restaurant.description"
-        class="form-control"
+        id="description"
+        class="form-control mt-2"
         rows="3"
         name="description"
       />
     </div>
 
-    <div class="form-group">
+    <div class="form-group my-3">
       <label for="image">Image</label>
       <img
         v-if="restaurant.image"
         :src="restaurant.image"
-        class="d-block img-thumbnail mb-3"
+        class="d-block img-thumbnail mt-2"
         width="200"
         height="200"
-      >
-
+      />
       <input
+        @change="handleFileChange"
         id="image"
         type="file"
         name="image"
         accept="image/*"
-        class="form-control-file"
-        @change="handleFileChange"
-      >
+        class="form-control mt-2"
+      />
     </div>
 
-    <button
-      type="submit"
-      class="btn btn-primary"
-    >
-      送出
+    <button type="submit" class="btn btn-primary my-3" :disabled="isProcessing">
+      {{ isProcessing ? "處理中" : "送出" }}
     </button>
     <button @click="$router.back()" class="btn btn-secondary my-3 mx-3">
       返回
@@ -112,41 +108,13 @@
 </template>
 
 <script>
-
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: '中式料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 2,
-      name: '日本料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 4,
-      name: '墨西哥料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  ]
-}
+import adminAPI from '../apis/admin.js'
+import { Toast } from '../utils/helpers.js'
 
 export default {
-   props: {
+  props: {
     initialRestaurant: {
       type: Object,
-      //當我們要為物件或陣列類型的資料設定預設值時，需要使用 () => ({}) 的寫法來回傳物件。
       default: () => ({
         name: "",
         categoryId: "",
@@ -157,47 +125,88 @@ export default {
         openingHours: "",
       }),
     },
-  },  
-  data () {
-    return {
-      restaurant: {
-        name: '',
-        categoryId: '',
-        tel: '',
-        address: '',
-        description: '',
-        image: '',
-        openingHours: ''
-      },
-      categories: []
+    isProcessing: {
+      type: Boolean,
+      default: false
     }
   },
-   created () {
-    //先用 fetchCategories 拿到類別，放進 this.categories   
-    this.fetchCategories();
+  data() {
+    return {
+      restaurant: {
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: "",
+      },
+      categories: [],
+      isLoading: true
+    }
+  },
+  watch: {
+    initialRestaurant(newValue) {
+      this.restaurant = {
+        ...this.restaurant,
+        ...newValue
+      }
+    }
+  },
+  created() {
+    this.fetchCategories()
     this.restaurant = {
       ...this.restaurant,
       ...this.initialRestaurant
     }
   },
-   methods: {
-    fetchCategories() {
-      this.categories = dummyData.categories;
+  methods: {
+    async fetchCategories() {
+      try {
+        const { data } = await adminAPI.categories.get()
+
+        if (data.categories.length === 0) {
+          throw new Error('error')
+        }
+        this.categories = data.categories
+        this.isLoading = false
+      } catch (error) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
     },
     handleFileChange(event) {
-      const files = event.target.files;
-      // console.log(files);
+      const files = event.target.files
+
       if (files.length === 0) {
-        this.restaurant.image = "";
+        this.restaurant.image = ""
       } else {
-        const imageURL = window.URL.createObjectURL(files[0]);
-        this.restaurant.image = imageURL;
+        const imageURL = window.URL.createObjectURL(files[0])
+        this.restaurant.image = imageURL
       }
     },
     handleFormSubmit(event) {
-      const form = event.target;
-      const formData = new FormData(form);
-      this.$emit("after-form-submit", formData);
+      // form validation
+      if (!this.restaurant.name) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請填寫餐廳名稱'
+        })
+        return
+      } else if (!this.restaurant.categoryId) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請選擇餐廳類別'
+        })
+        return
+      }
+
+      const form = event.target
+      const formData = new FormData(form)
+      this.$emit("after-form-submit", formData)
     },
   },
 };
