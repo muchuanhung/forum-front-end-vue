7b30a8505896352aadbb88e056d6c90e2a835bc2
muchuan <mu.chuan.hung@gmail.com>
2021-11-30 22:01:29 +0800

    refractor: 重構api helper

--- src/apis/users.js
+++ src/apis/users.js
@@ -2,9 +2,6 @@
 import { apiHelper } from '../utils/helpers'
 
 export default {
-  getCurrentUser() {
-    return apiHelper.get(`/get_current_user`,)
-  },
   getUser({ userId }) {
     return apiHelper.get(`/users/${userId}`,)
   },
