dcb045956ba1d54c3f1a7dd0491fed9dbc5f9131
muchuan <mu.chuan.hung@gmail.com>
2021-11-30 14:53:16 +0800

    refractor: 編輯使用者名稱與大頭照

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,46 +1,99 @@
 <template>
   <div class="container py-5">
-    <UserEditForm 
-     :initial-profile="userProfile"
-     @after-submit="handleAfterSubmit" 
+    <UserEditForm
+      v-if="isCurrentUser || currentUser.isAdmin"
+      :initial-profile="userProfile"
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
     />
-  </div>
+     <div v-else>
+      <div class="fs-5">抱歉，您無法編輯此用戶資料！</div>
+      <button
+        @click.prevent.stop="
+          $router.push({ name: 'user', params: userProfile.id })
+        "
+        class="btn btn-secondary my-3"
+      >
+        返回使用者頁面
+      </button>
+    </div>
+</div>
 </template>
 
 <script>
-import UserEditForm from "../components/UserEditForm.vue";
-
-//設定種子資料
-const dummyData = {
-  profile: {
-    id: 1,
-    name: "root",
-    email: "root@example.com",
-    image: "https://i.imgur.com/58ImzMM.png",
-    updatedAt: "2019-08-01T10:33:51.095Z",
-  },
-};
+import UserEditForm from '../components/UserEditForm.vue'
+import usersAPI from '../apis/users.js'
+import { Toast } from '../utils/helpers.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     UserEditForm,
   },
-
   data() {
     return {
-      userProfile: dummyData.profile,
-    };
+  userProfile: {
+        id: -1,
+        name: '',
+        email: '',
+        image: ''
+      },
+      //當送出表單但伺服器還沒回應前，避免使用者重複點擊送出按鈕
+      isProcessing: false
+    }
   },
-
-
-   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ": " + value);
+  computed: {
+    ...mapState(['currentUser', 'isAuthenticated']),
+    isCurrentUser() {
+      const userProfileId = this.$route.params.id
+      return this.currentUser.id == userProfileId && this.isAuthenticated
+    }
+  },
+  created() {
+    const { id } = this.$route.params
+    this.fetchUserData(id)
+  },
+  methods: {
+   async fetchUserData(userId) {
+      try {
+        const { data } = await usersAPI.getUser({ userId })
+        if (data.profile.name.length === 0) {
+          throw new Error('error')
+        }
+        this.userProfile = {
+          ...this.userProfile,
+          ...data.profile
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得用戶資料，請稍後再試'
+        })
+      }
+    },
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const userId = this.userProfile.id
+        //更新使用者資料
+        const { data } = await usersAPI.updateUser({ userId, formData })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        this.$router.push({ name: 'user', params: userId })
+        // console.log("-- 透過 API 傳送資料到後端伺服器 --")
+        // for (let [name, value] of formData.entries()) {
+        //   console.log(name + ": " + value)
+        // }
+      } catch (error) {
+        this.isProcessing = false
+        //使用者若漏填姓名則發出 Toast 提示
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新使用者資料，請稍後再試'
+        })
       }
     },
   },
-}
+};  
 </script>
-
